ffb1e7f2e4fa260c3bc18636330b47a54b4a6169
Nemial <nemialtred@yandex.ru>
2022-08-17 09:13:42 +0200

    refactor: use Identity for check admin group

--- templates/Users/edit.php
+++ templates/Users/edit.php
@@ -45,7 +45,7 @@
                 ) ?>
             </div>
             <?php
-            if ($user->is_admin): ?>
+            if ($this->Identity->get('is_admin')): ?>
                 <div class="mb-3">
                     <label for="groups" class="form-label"><?= __('Группы') ?></label>
                     <?= $this->Form->select(
